6d24539a8eda274c31813f1d649c561ef1b9611a
bennykok <itechbenny@gmail.com>
2025-06-16 11:16:26 +0800

    fix commit

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cd-misc.s3.us-east-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cd-public-misc.s3.us-east-1.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default withMDX(config);
