76075e47e0c34c3eeb674838c000050be6b18cc0
KarrixLee <thomasdice1834793903@gmail.com>
2025-06-25 00:01:26 +0800

    Revert "fix sidebar tabs and update metadata"
    This reverts commit 4597201dd1d59943b17569efb6fcdaf08f3880b1.

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 import type { PageTree } from 'fumadocs-core/server';
-import { getSidebarTabs } from 'fumadocs-ui/utils/get-sidebar-tabs';
+import { FileText, Link } from 'fumadocs-ui/internal/icons';
 
 function filterTree(tree: PageTree.Root, api: boolean): PageTree.Root {
   return {
@@ -26,11 +26,22 @@ export default function Layout({
   const isApi = slug === 'api-docs';
 
   const tree = filterTree(source.pageTree, isApi);
-  const tabs = getSidebarTabs(source.pageTree, {
-    transform(option) {
-      return { ...option, props: { className: 'fd-tab-item' } };
+  const tabs = [
+    {
+      url: '/docs/introduction',
+      title: 'Dashboard',
+      icon: <FileText className="size-4" />,
+      description: 'Guides',
+      props: { className: 'fd-tab-item' },
     },
-  });
+    {
+      url: '/docs/api-docs/run/deployment-queue',
+      title: 'API',
+      icon: <Link className="size-4" />,
+      description: 'API Reference',
+      props: { className: 'fd-tab-item' },
+    },
+  ];
 
   return (
     <DocsLayout tree={tree} {...baseOptions} sidebar={{ tabs }}>
